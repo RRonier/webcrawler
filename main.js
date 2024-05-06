ac9c623e758a8d77e083b4cdd702fc23a4df5de3
Ronier Ramos <ronier2332@gmail.com>
2024-05-06 15:41:41 +0200

    add report of visited links

--- main.js
+++ main.js
@@ -1,21 +1,19 @@
-const {crawlPage} = require("./crawler");
+const { crawlPage } = require("./crawler");
+const { printReport } = require('./report')
 
 async function main() {
-    if(process.argv.length < 3) {
+    if (process.argv.length < 3) {
         console.log("no website provided")
         process.exit(1)
     }
-    if(process.argv.length > 3) {
+    if (process.argv.length > 3) {
         console.log("too many command line args")
         process.exit(1)
     }
     const baseURL = process.argv[2]
     console.log(`starting crawl of ${baseURL}`)
     const pages = await crawlPage(baseURL, baseURL,{})
-
-    for(const page of Object.entries(pages)) {
-        console.log(page)
-    }
+    printReport(pages)
 }
 
 main()
